8c0bad12e5fb54e857b85ebddee9baab7ca054e1
Reda Mimouni <rmimouni@octo.com>
2017-08-29 12:12:52 +0200

    Adding custom cell

---
+++ NfcReader/NfcReader/NFCTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NFCTableViewCell.swift
+//  NfcReader
+//
+//  Created by Reda Mimouni on 29/08/2017.
+//  Copyright © 2017 Reda Mimouni. All rights reserved.
+//
+
+import UIKit
+
+class NFCTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- NfcReader/NfcReader/NFCTableViewController.swift
+++ NfcReader/NfcReader/NFCTableViewController.swift
@@ -7,89 +7,89 @@
 //
 
 import UIKit
+import CoreNFC
 
 class NFCTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  // Reference the NFC session
+  private var nfcSession: NFCNDEFReaderSession!
+  
+  // Reference the found NFC messages
+  private var nfcMessages: [[NFCNDEFMessage]] = []
+  
+  // Start the search when tapping the "Start Search" button
+  @IBAction func startNFCSearchButtonTapped(_ sender: Any) {
+    self.nfcSession.begin()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Register our table-cell to display the records
+    self.tableView.register(NFCTableViewCell.self, forCellReuseIdentifier: "NFCTableCell")
+    
+    // Create the NFC Reader Session when the app starts
+    self.nfcSession = NFCNDEFReaderSession(delegate: self, queue: DispatchQueue.main, invalidateAfterFirstRead: false)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 0
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return 0
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    
+    // Configure the cell...
+    
+    return cell
+  }
+  
 }
+
+
+// MARK: NFCNDEFReaderSessionDelegate
+
+extension NFCTableViewController : NFCNDEFReaderSessionDelegate {
+  
+  // Called when the reader-session expired, you invalidated the dialog or accessed an invalidated session
+  func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+    print("NFC-Session invalidated: \(error.localizedDescription)")
+  }
+  
+  // Called when a new set of NDEF messages is found
+  func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+    print("New NFC Messages (\(messages.count)) detected:")
+    
+    for message in messages {
+      print(" - \(message.records.count) Records:")
+      for record in message.records {
+        print("\t- TNF (TypeNameFormat): \(NFCTableViewController.formattedTypeNameFormat(from: record.typeNameFormat))")
+        print("\t- Payload: \(String(data: record.payload, encoding: .utf8)!)")
+        print("\t- Type: \(record.type)")
+        print("\t- Identifier: \(record.identifier)\n")
+      }
+    }
+    
+    // Add the new messages to our found messages
+    self.nfcMessages.append(messages)
+    
+    // Reload our table-view on the main-thread to display the new data-set
+    DispatchQueue.main.async {
+      self.tableView.reloadData()
+    }
+  }
+}
+
